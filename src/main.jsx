82fbcd785cfa45e7541d2cb1d3f6b33f73e007c2
Suzi Nogueira <12518149+suzinogueira@users.noreply.github.com>
2025-11-04 15:07:15 -0300

    Uncomment React import statements and render method

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+/*import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./App.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
